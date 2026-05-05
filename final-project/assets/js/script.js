32802648fe3aa530c3e600bc80454cf8f53ac96b
Kyle Johnson <157499502+kyjohnson05@users.noreply.github.com>
2026-05-05 06:52:23 +0000

    Finished phone breakpoint of website; included progressive enhancement, responsive images, and decorated site with CSS and JS

---
+++ final-project/assets/js/script.js
@@ -0,0 +1,18 @@
+document.documentElement.classList.add("js");
+
+var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for(i = 0; i < acc.length ; i++) {
+        acc[i].addEventListener("click", function (){
+            this.classList.toggle("active");
+            this.parentElement.classList.toggle("active");
+
+            var pannel = this.nextElementSibling;
+
+            const expanded = this.getAttribute("aria-expanded") === "true";
+            this.setAttribute("aria-expanded", !expanded);
+
+            pannel.classList.toggle("open");
+        });
+    }
